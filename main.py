47ff82bfb9f01f97725a08a2e055fe0912105280
Gabriel Nugent <g.nugent@techsmith.com>
2025-01-28 17:43:23 -0500

    Concept created

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+from typing import Annotated
 from resemblyzer import preprocess_wav, VoiceEncoder
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
+from fastapi import FastAPI, UploadFile, File
 
 encoder = VoiceEncoder()
 
-wavFilePaths = list(Path("./audioFiles").glob("*.wav"))
-wavs = np.array(list(map(preprocess_wav, tqdm(wavFilePaths, "Preprocessing wavs.", len(wavFilePaths)))), dtype=object)
+app = FastAPI()
 
-utteranceEmbedA = encoder.embed_utterance(wavs[0])
-utteranceEmbedB = encoder.embed_utterance(wavs[1])
+async def writeFile(file: UploadFile, filename: str):
+    with open(filename, "wb") as f:
+        f.write(await file.read())
 
-utteranceSimilarityMatrix = np.dot(utteranceEmbedA, utteranceEmbedB) / (np.linalg.norm(utteranceEmbedA) * np.linalg.norm(utteranceEmbedB))
+@app.post("/verify-voice", status_code=200)
+async def verifyVoice(verification: UploadFile = File(...), cloning: UploadFile = File(...)):
+    await writeFile(verification, "verification.wav")
+    await writeFile(cloning, "cloning.wav")
 
-print(utteranceSimilarityMatrix)
+    verificationWav = preprocess_wav("verification.wav")
+    cloningWav = preprocess_wav("cloning.wav")
+
+    utteranceEmbedA = encoder.embed_utterance(verificationWav)
+    utteranceEmbedB = encoder.embed_utterance(cloningWav)
+
+    utteranceSimilarityScore: float = np.dot(utteranceEmbedA, utteranceEmbedB) / (np.linalg.norm(utteranceEmbedA) * np.linalg.norm(utteranceEmbedB))
+    return {'similarityScore': float(utteranceSimilarityScore)}
